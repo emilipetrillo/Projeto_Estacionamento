0fc20f105469bbb64e3efaf832ff24df1f0d2416
emilipetrillo <emili.mionpetrillo@gmail.com>
2017-11-14 21:59:04 -0200

    funcionando_masTrava

---
+++ Projeto_Estacionamento/20171031_Projeto_Estacionamento_MQTT/20171031_Projeto_Estacionamento_MQTT.ino
@@ -0,0 +1,203 @@
+//// incluindo as bibliotecas
+#include <PubSubClient.h> // MQTT
+#include <SPI.h> // comunicacao
+#include <UIPEthernet.h> // ethernet
+#include <utility/logging.h> //ethernet
+#include <Ultrasonic.h>
+
+// declarando as variáveis
+const int greenPin = 2; // colocar a porta
+const int redPin = 4;
+const int yellowPin = A1;
+int distance;
+int prevDistance;
+int statusLedAtual;
+int statusLedAnterior;
+
+
+// setando as configuracoes do MQTT
+byte mac[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0x14 };
+//const char* mqtt_server = "m12.cloudmqtt.com";
+int port = 1883;
+IPAddress server(192, 168, 3, 186);
+
+// setando as configurações do sensor de distnacia
+Ultrasonic ultrasonic(7, 8);
+
+
+// função callback - para receber as informações dos tópicos
+void callback (char* topic, byte* payload, unsigned int length, boolean retained) {
+  String topicStr = String(topic);
+  Serial.print("Vagas disponíveis: ");
+  Serial.print(topicStr);
+  Serial.println();
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Muda o led que vai acender conforme a vaga estiver ou não vazia.
+  // Se a vaga estiver vazia - acende led verde
+  // seão acende a vermelha - ocupada
+  if ((char)payload[0] == '1') {
+    digitalWrite(greenPin, HIGH);
+    digitalWrite(redPin, LOW);
+    Serial.println("Vaga livre");
+  } else if ((char)payload[0] == '0') {
+    digitalWrite(greenPin, LOW);
+    digitalWrite(redPin, HIGH);
+    Serial.println("Vaga ocupada");
+  }
+}
+
+EthernetClient ethClient;
+PubSubClient mqttClient (server, port, callback, ethClient);
+
+
+void setup() {
+  // seta os pinos leds
+  pinMode (greenPin, OUTPUT);
+  pinMode (redPin, OUTPUT);
+  pinMode (yellowPin, OUTPUT);
+
+  // tenta conexão com a internet
+  Serial.begin(9600);
+  while (!Serial) { }
+
+  Serial.println("Tentando conexão via DHCP/IP...");
+  //    Serial.println("teeeesssteee leeddd");
+  //    digitalWrite(yellowPin, HIGH);
+  //    delay(100);
+  //    digitalWrite(yellowPin, LOW);
+  //    delay(100);
+
+
+  // tentando conectar via DHCP, se falhar tenta por IP
+  if (!Ethernet.begin(mac)) {
+    Serial.println("DHCP falhou");
+  } else {
+    Serial.print("Conectado no IP: ");
+    Serial.println(Ethernet.localIP());
+    delay(2000);
+  }
+
+  reconnectMQTT();
+  /*
+    // tentando conectar no MQTT
+    if (mqttClient.connect("ClientId", "teste", "123", "Vaga", 0, true, "")) {
+      // ClientID - nome unico
+      // teste - nome do user no mqtt
+      // 123 - senha
+      // Vaga - nome do topico no mqtt
+      // 0 - default
+      // true - para ativar a função do retain
+      // "" - apaga todas as mensagens quando for desligado abruptamente
+      Serial.println("Conectado com MQTT");
+      mqttClient.publish("Vaga", "1", true); // reportando vaga disponível. O true faz a função do retain
+      //mqttClient.publish("TopicWill", "0", false); // replicando a msg do broker
+      mqttClient.subscribe("Vaga");
+      mqttClient.setCallback(callback);
+      Serial.flush();
+    } else {
+      Serial.println("Não conectado com MQTT");
+    }
+  */
+}
+
+void loop() {
+
+  Ethernet.maintain();
+  // se perder a conexão com o MQTT tenta reconectar
+  if (!mqttClient.connected()) {
+    Serial.println("Conexão com MQTT perdida.");
+    delay(2 * 1000);
+    reconnectMQTT();
+  }
+  //  mqttClient.loop();
+
+
+  // le a distancia
+  distance = ultrasonic.distanceRead();
+  Serial.print("Distancia Atual: ");
+  Serial.println(distance);
+
+  //  if (distance && (prevDistance > 6)) {
+  // acende o led vermelho se a distancia for menor que 6,
+  // acende o led ver se a distancia for maior ou igual a 6.
+  if (distance >= 6) {
+    digitalWrite(greenPin, 1);
+    digitalWrite(redPin, 0);
+    delay(100);
+  } else {
+    digitalWrite(greenPin, 0);
+    digitalWrite(redPin, 1);
+    delay(100);
+  }
+  statusLedAtual = digitalRead(greenPin);
+
+  if (statusLedAtual != statusLedAnterior) {
+    if (statusLedAtual == 1) {
+      Serial.println("Vaga disponível");
+      mqttClient.publish("vagas/14", "1", true);
+    } else {
+      Serial.println("Vaga indisponível");
+      mqttClient.publish("vagas/14", "0", true);
+    }
+    statusLedAnterior = statusLedAtual;
+    delay(100);
+  }
+  mqttClient.loop();
+  //loop();
+}
+
+
+
+// essa função fica tentando se reconectar ao MQTT.
+void reconnectMQTT() {
+  digitalWrite(yellowPin, LOW);
+  while (!mqttClient.connected()) {
+
+    Serial.println("Tentando reconectar MQTT...");
+    
+    digitalWrite(yellowPin, HIGH);
+    delay(100);
+    digitalWrite(yellowPin, LOW);
+    delay(100);
+    digitalWrite(yellowPin, HIGH);
+    delay(100);
+    digitalWrite(yellowPin, LOW);
+    delay(100);
+    
+    if (mqttClient.connect("ClientId", "teste", "123", "vagas", 0, true, "")) {
+      // ClientID - nome unico
+      // teste - nome do user no mqtt
+      // 123 - senha
+      // Vaga - nome do topico no mqtt
+      // 0 - default
+      // true - para ativar a função do retain
+      // "" - apaga todas as mensagens quando for desligado abruptamente
+      Serial.println("Conectado com MQTT");
+      Serial.println("Conectado");
+      delay(2 * 1000);
+      // envia via mqtt uma mensagem de conectado
+      mqttClient.publish("vagas/14", "1", true);
+      mqttClient.subscribe("vagas/14");
+      mqttClient.setCallback(callback);
+      Serial.flush();
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(mqttClient.state());
+      Serial.println("Tentará reconecta em 2 segundos");
+      delay(2 * 1000);
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
---
+++ Projeto_Estacionamento/placa/placa.ino
@@ -0,0 +1,180 @@
+// include the library code:
+#include <LiquidCrystal.h>
+
+//#include <Ethernet.h>
+#include <SPI.h>
+#include <UIPEthernet.h>
+#include <utility/logging.h>
+#include <PubSubClient.h>
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 7, en = 6, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+int vagasDisponiveis = 0;
+int vagasOcupadas = 0;
+long tempoInicial;
+int redpin = A0;    //Pin A0
+int greenpin = 9;  //Pin 9
+int bluepin = 8;    //Pin 8
+
+int vagas[12] = {};
+int msgComoNumero;
+int index;
+
+//// Atualizar ultimo valor para ID do seu Kit para evitar duplicatas
+//byte mac[] = { 0xDE, 0xED, 0xBA, 0xFE, 0xF1, 0x55 };
+//// Endereço do Cloud MQTT
+//char* server = "m12.cloudmqtt.com";
+//// Valor da porta do servidor MQTT
+//int port = 12397;
+
+byte mac[] = { 0xDE, 0xED, 0xBA, 0xFE, 0xF1, 0x55 };
+int port = 1883;
+IPAddress server(192, 168, 3, 186);
+EthernetClient ethClient;
+
+
+// FUNÇÃO que irá receber o retorno do servidor.
+void whenMessageReceived(char* topic, byte* payload, unsigned int length) {
+  // Converter pointer do tipo `byte` para typo `char`
+  char* payloadAsChar = payload;
+
+  // Converter em tipo String para conveniência
+  String msg = String(payloadAsChar);
+  Serial.print("Topic received: "); Serial.println(topic);
+
+  // Dentro do whenMessageReceived (callback) da biblioteca MQTT,
+  // devemos usar Serial.flush() para garantir que as mensagens serão enviadas
+  Serial.flush();
+
+  tempoInicial = millis(); // somente se quiser resetar o tempo
+  lcd.display();
+
+  msgComoNumero = msg.toInt();
+
+  Serial.print("Numero lido: "); Serial.println(msgComoNumero);
+  Serial.flush();
+
+  String topicID = String(topic[12]);
+  topicID += String(topic[13]);
+  index = topicID.toInt();
+
+
+  vagas[index] = msgComoNumero;
+  
+}
+
+void atualizaVagas() {
+  vagasOcupadas = 0;
+  vagasDisponiveis = 0;
+
+  for (int i = 0; i < 12; i++) {
+    if (vagas[i] == 0) {
+      vagasDisponiveis = vagasDisponiveis + 1;
+    } else {
+      vagasOcupadas = vagasOcupadas + 1;
+    }
+
+  }
+
+  lcd.setCursor(12, 0);
+  lcd.print("    ");
+  lcd.setCursor(12, 0);
+  lcd.print(vagasDisponiveis);
+  lcd.setCursor(12, 1);
+  lcd.print("    ");
+  lcd.setCursor(12, 1);
+  lcd.print(vagasOcupadas);
+}
+
+
+// Dados do MQTT Cloud
+PubSubClient client(server, port, whenMessageReceived, ethClient);
+
+void setup() {
+
+  Serial.println("Pronto");
+
+  Serial.begin(9600);
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  lcd.clear();
+  lcd.print("Iniciando...");
+
+  pinMode(bluepin, OUTPUT);
+  pinMode(greenpin, OUTPUT);
+  pinMode(redpin, OUTPUT);
+
+
+  Serial.println("Connecting...");
+
+  while (!Serial) {}
+
+  if (!Ethernet.begin(mac)) {
+    Serial.println("DHCP Failed");
+  } else {
+    Serial.println(Ethernet.localIP());
+  }
+
+  // Faz a conexão no cloud com nome do dispositivo, usuário e senha respectivamente
+  reconnect();
+
+  lcd.setCursor(0, 0);
+  lcd.print("Disponiveis:");
+  lcd.setCursor(0, 1);
+  lcd.print("Ocupadas   :");
+
+  //atualizaVagas();
+  Serial.println("VVVVVVVVVVVVVVVVVVVVVVVaaaaa");
+}
+
+void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+  }
+
+  // A biblioteca PubSubClient precisa que este método seja chamado em cada iteração de `loop()`
+  // para manter a conexão MQTT e processar mensagens recebidas (via a função callback)
+  client.loop();
+
+  if (millis() >= (tempoInicial + 10000)) {
+    lcd.noDisplay();
+  }
+
+  if (vagasDisponiveis == 0) {
+    analogWrite(bluepin, 250);   //MAGENTA
+    analogWrite(redpin, 255);
+    //    analogWrite(greenpin, 9);
+  } else {
+    analogWrite(redpin, 0);
+    analogWrite(greenpin, 0);
+    analogWrite(bluepin, 0);
+  }
+
+  delay(100);
+  atualizaVagas();
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    //if (client.connect("id23", "codexpiot", "iot")) {
+    if (client.connect("id23")) {
+      //Conecta no topic para receber mensagens
+      client.subscribe("vagas/+");
+      //      client.subscribe("vaga");
+      Serial.println("conectado topico");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 4 seconds");
+      // Wait 5 seconds before retrying
+      delay(4000);
+    }
+  }
+}
